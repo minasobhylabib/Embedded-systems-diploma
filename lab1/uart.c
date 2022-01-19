0d5cc72cb140ebe4f416a47d060ff608ca6b0a72
mina.sobhy.labib@gmail.com <mina.sobhy.labib@gmail.com>
2022-01-20 00:49:25 +0200

    uart.c

---
+++ lab1/uart.c
@@ -0,0 +1,11 @@
+#include "uart.h"
+#define UARTODR *((volatile unsigned int*)((unsigned int*)0x101f1000))
+
+void uart_send_string(unsigned char* P_tx_string)
+{
+	while(*P_tx_string != '\0')
+	{
+		UARTODR = (unsigned int )(*P_tx_string);
+		P_tx_string++;
+	}
+}
